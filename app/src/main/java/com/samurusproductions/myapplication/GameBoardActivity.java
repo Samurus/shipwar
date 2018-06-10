8d2f821af2d7af9ba8939e0cc3140c07045a4052
samurus <conann233@web.de>
2018-06-10 14:16:13 +0200

    init

---
+++ app/src/main/java/com/samurusproductions/myapplication/GameBoardActivity.java
@@ -0,0 +1,22 @@
+package com.samurusproductions.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Julius on 10.06.2018.
+ */
+
+public class GameBoardActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_board);
+
+
+
+
+}}
